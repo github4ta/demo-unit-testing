23feb24156c74f30d1a1f3ceade99597e5bc0939
trainingby1 <trainingby1@google.com>
2022-11-05 11:17:40 +0300

    add testAdd method

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalculatorTest {
+
+    @Test
+    public void testSum() {
+        // TODO GIVEN - ASSEMBLY
+        Calculator calculator = new Calculator();
+        int a = 2;
+        int b = 2;
+        int expected = 4;
+
+        // TODO WHEN - ACT
+        int actual = calculator.sum(a, b);
+
+        // TODO THEN - ASSERT
+        Assert.assertEquals(expected, actual);
+    }
 }
