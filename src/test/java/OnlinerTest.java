f94b0494c551a0f24d4feed0790d63e59fc3a649
trainingby1 <trainingby1@google.com>
2022-11-05 12:17:01 +0300

    refactor Test class

--- src/test/java/OnlinerTest.java
+++ src/test/java/OnlinerTest.java
@@ -1,6 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlinerTest {
@@ -11,11 +14,17 @@ public class OnlinerTest {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         String url = "http://www.onliner.by";
-        String xpath = "";
+        String xpath = "//div[@class='footer-style__copy']";
 
         // TODO WHEN
         driver.get(url);
+        WebElement element = driver.findElement(By.xpath(xpath));
+        Util.waiter();
+        String actual = element.getText();
 
-
+        // TODO THEN
+        System.out.println(actual);
+        driver.quit();
+        Assert.fail();
     }
 }
