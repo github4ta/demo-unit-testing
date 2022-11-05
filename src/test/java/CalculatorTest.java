23e856e0cac1385992d838fbc3ab27513183ec90
Maria Leshkova <waderfg90@gmail.com>
2022-11-05 11:46:27 +0300

    add some more junit tests!

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -16,5 +16,53 @@ public class CalculatorTest {
 
         // TODO THEN - ASSERT
         Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testMultiply() {
+        // TODO GIVEN - ASSEMBLY
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 7;
+        int expected = 35;
+
+        // TODO WHEN - ACT
+        int actual = calculator.multiply(a, b);
+
+        // TODO THEN - ASSERT
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSubtract() {
+        // TODO GIVEN - ASSEMBLY
+        Calculator calculator = new Calculator();
+        int a = 13;
+        int b = 11;
+        int expected = 2;
+
+        // TODO WHEN - ACT
+        int actual = calculator.subtract(a, b);
+
+        // TODO THEN - ASSERT
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDivide() {
+        // TODO GIVEN - ASSEMBLY
+        Calculator calculator = new Calculator();
+        int a = 40;
+        int b = 5;
+        int expected = 8;
+
+        // TODO WHEN - ACT
+        int actual = calculator.divide(a, b);
+
+        // TODO THEN - ASSERT
+        Assert.assertEquals(expected, actual);
     }
 }
+
+
