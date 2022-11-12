3b40bebffbaa13d84d5c8e02404d92fa766a99f1
irina <irisha.dieriecha@mail.ru>
2022-11-12 10:11:44 +0300

    add page object calculator

--- src/test/java/WeightCalculatorTest.java
+++ src/test/java/WeightCalculatorTest.java
@@ -1,2 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WeightCalculatorTest {
+    @Test
+    public void testOpenOnlinerWebSite() {
+        // TODO GIVEN
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        String url = "https://svyatoslav.biz/testlab/wt/index.php";
+        String xpathInputName = "//input[@name='name']";
+        String xpathInputHeight = "//input[@name='height']";
+        String xpathInputWeight = "//input[@name='weight']";
+        String xpathInputGenderMan = "//input[@value='m']";
+        String xpathButtonSubmit = "//input[@type='submit']";
+        String xpathResultText = "//tbody/tr[2]/td[2]";
+        String expected = "Идеальная масса тела";
+
+        // TODO WHEN
+        driver.get(url);
+        By byXpath = By.xpath(xpath);
+        WebElement element = driver.findElement(byXpath);
+        Util.waiter();
+        String actual = element.getText();
+
+        // TODO THEN
+        Assert.assertEquals(expected, actual);
+        driver.quit();
+    }
 }
