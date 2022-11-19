bf1c460c57924a34d96550aa31347369311f416b
Maria Leshkova <waderfg90@gmail.com>
2022-11-19 10:30:01 +0300

    add login form test (for empty)

--- src/test/java/OnlinerTest.java
+++ src/test/java/OnlinerTest.java
@@ -28,6 +28,7 @@ public class OnlinerTest {
         Assert.assertEquals(expected, actual);
         driver.quit();
     }
+
     @Test
     public void testOpenBaraholkaPage() {
 
@@ -53,6 +54,51 @@ public class OnlinerTest {
 
         // выполнить проверку текстов
         Assert.assertEquals(expected, actual);
+        driver.quit();
+    }
+
+    @Test
+    public void testOpenOnlinerLoginPage() {
+        // TODO GIVEN
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        String url = "http://www.onliner.by";
+        String xpathButtonGoToLoginPage = "//*[@id='userbar']/div[1]/div/div/div[1]";
+        String xpathButtonLogin = "//*[@id='auth-container']/div/div[2]/div/form/div[3]/button";
+        driver.get(url);
+        Util.waiter();
+        By byXpathButtonGoToLoginPage = By.xpath(xpathButtonGoToLoginPage);
+        WebElement elementButtonGoToLoginPage = driver.findElement(byXpathButtonGoToLoginPage);
+        elementButtonGoToLoginPage.click();
+        Util.waiter();
+
+        String expectedErrorMessageUserName="Укажите ник или e-mail";
+        String expectedErrorMessagePassword="Укажите пароль";
+
+        // TODO WHEN
+        By byXpathButtonLogin = By.xpath(xpathButtonLogin);
+        WebElement elementButtonLogin = driver.findElement(byXpathButtonLogin);
+        elementButtonLogin.click();
+        Util.waiter();
+
+        String xpathErrorMessageUserName = "//*[@id='auth-container']/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
+        By byXpathErrorMessageUserName = By.xpath(xpathErrorMessageUserName);
+        WebElement elementErrorMessageUserName = driver.findElement(byXpathErrorMessageUserName);
+
+
+        String xpathErrorMessagePassword = "//*[@id='auth-container']/div/div[2]/div/form/div[2]/div/div/div[2]/div";
+        By byXpathErrorMessagePassword = By.xpath(xpathErrorMessagePassword);
+        WebElement elementErrorMessagePassword = driver.findElement(byXpathErrorMessagePassword);
+
+        String actualErrorMessageUserName=elementErrorMessageUserName.getText();
+        String actualErrorMessagePassword=elementErrorMessagePassword.getText();
+
+
+
+        // TODO THEN
+        Assert.assertEquals(expectedErrorMessageUserName, actualErrorMessageUserName);
+        Assert.assertEquals(expectedErrorMessagePassword, actualErrorMessagePassword);
+        Util.waiter();
         driver.quit();
     }
 }
