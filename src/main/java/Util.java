4dfe8c3cfedc41b05db1988ba766711907e98a10
trainingby1 <trainingby1@google.com>
2022-11-26 09:33:11 +0300

    add waiters

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,8 +1,8 @@
 public class Util {
 
-    public static void waiter() {
+    public static void waiter(int timeSeconds) {
         try {
-            Thread.sleep(5000);
+            Thread.sleep(timeSeconds * 1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
--- src/test/java/OnlinerTest.java
+++ src/test/java/OnlinerTest.java
@@ -1,32 +1,51 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class OnlinerTest {
+    WebDriver driver;
+
+    @Before
+    public void beforeTest() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        //driver.manage().timeouts().implicitlyWait(1, TimeUnit.MILLISECONDS);
+    }
+
+    @After
+    public void afterTest() {
+        driver.quit();
+    }
+
+
 
     @Test
     public void testOpenOnlinerWebSite() {
         // TODO GIVEN
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
         String url = "http://www.onliner.by";
         String xpath = "//div[@class='footer-style__copy']";
-        String expected = "© 2001—2022 Onlíner";
+        final String FOOTER_COPY = "© 2001—2022 Onlíner";
 
         // TODO WHEN
         driver.get(url);
         By byXpath = By.xpath(xpath);
         WebElement element = driver.findElement(byXpath);
-        Util.waiter();
+        Util.waiter(1);
         String actual = element.getText();
 
         // TODO THEN
-        Assert.assertEquals(expected, actual);
-        driver.quit();
+        Assert.assertEquals(FOOTER_COPY, actual);
     }
 
     @Test
@@ -48,7 +67,7 @@ public class OnlinerTest {
         // получить текст из веб элемента
         WebElement elementBaraholkaLink = driver.findElement(By.xpath(xpathBaraholkaLink));
         elementBaraholkaLink.click();
-        Util.waiter();
+        Util.waiter(3);
         WebElement elementBaraholkaTitle = driver.findElement(By.xpath(xpathBaraholkaTitle));
         String actual = elementBaraholkaTitle.getText();
 
@@ -60,45 +79,39 @@ public class OnlinerTest {
     @Test
     public void testOpenOnlinerLoginPage() {
         // TODO GIVEN
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
         String url = "http://www.onliner.by";
         String xpathButtonGoToLoginPage = "//*[@id='userbar']/div[1]/div/div/div[1]";
         String xpathButtonLogin = "//*[@id='auth-container']/div/div[2]/div/form/div[3]/button";
+        String expectedErrorMessageUserName="Укажите ник или e-mail";
+        String expectedErrorMessagePassword="Укажите пароль";
+        String xpathErrorMessageUserName = "//*[@id='auth-container']/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
+        String xpathErrorMessagePassword = "//*[@id='auth-container']/div/div[2]/div/form/div[2]/div/div/div[2]/div";
+
         driver.get(url);
-        Util.waiter();
+
         By byXpathButtonGoToLoginPage = By.xpath(xpathButtonGoToLoginPage);
         WebElement elementButtonGoToLoginPage = driver.findElement(byXpathButtonGoToLoginPage);
         elementButtonGoToLoginPage.click();
-        Util.waiter();
-
-        String expectedErrorMessageUserName="Укажите ник или e-mail";
-        String expectedErrorMessagePassword="Укажите пароль";
 
         // TODO WHEN
         By byXpathButtonLogin = By.xpath(xpathButtonLogin);
         WebElement elementButtonLogin = driver.findElement(byXpathButtonLogin);
         elementButtonLogin.click();
-        Util.waiter();
 
-        String xpathErrorMessageUserName = "//*[@id='auth-container']/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
+        // TODO NoSuchElementException
         By byXpathErrorMessageUserName = By.xpath(xpathErrorMessageUserName);
-        WebElement elementErrorMessageUserName = driver.findElement(byXpathErrorMessageUserName);
+        Duration duration = Duration.ofSeconds(1);
+        WebElement elementErrorMessageUserName = (new WebDriverWait(driver, duration))
+                .until(ExpectedConditions.elementToBeClickable(byXpathErrorMessageUserName));
 
-
-        String xpathErrorMessagePassword = "//*[@id='auth-container']/div/div[2]/div/form/div[2]/div/div/div[2]/div";
         By byXpathErrorMessagePassword = By.xpath(xpathErrorMessagePassword);
         WebElement elementErrorMessagePassword = driver.findElement(byXpathErrorMessagePassword);
 
         String actualErrorMessageUserName=elementErrorMessageUserName.getText();
         String actualErrorMessagePassword=elementErrorMessagePassword.getText();
 
-
-
         // TODO THEN
         Assert.assertEquals(expectedErrorMessageUserName, actualErrorMessageUserName);
         Assert.assertEquals(expectedErrorMessagePassword, actualErrorMessagePassword);
-        Util.waiter();
-        driver.quit();
     }
 }
